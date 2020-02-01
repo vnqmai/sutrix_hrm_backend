0a361984069dfe065b1f9db527988ef2b1332b4e
vnqmai <vnqmai01@gmail.com>
2020-02-01 23:04:50 +0700

    remove config

--- config/index.js
+++
@@ -1,7 +0,0 @@
-var configValues = require('../config/cfg.json');
-
-module.exports = {
-    getDbConnectionString: function() {
-        return `mongodb+srv://${configValues.username}:${configValues.password}@testdb01-qmmsa.mongodb.net/sutrix_hrm`;
-    }
-}
